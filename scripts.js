bfdd6e2d04b46fee1623557860c0780e4431d89b
joechesney <33613119+joechesney@users.noreply.github.com>
2017-12-02 22:23:04 -0600

    the most basic site ever

---
+++ scripts.js
@@ -0,0 +1,7 @@
+/* These 2 funtions are designed to give an image a border when clicked on, then remove when the mouse is moved off the element. */
+function makeBorder() {
+    document.getElementById("bordy").style.border = "3px solid red";
+}
+function rmvBorder() {
+    document.getElementById("bordy").style.border = "none";
+}
